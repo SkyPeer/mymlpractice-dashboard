0d6578c24dfb29e1dd1458757879442aad39e5d9
Oleg Selivantsev <o.selivancev@gmail.com>
2026-01-28 01:48:17 +0400

    Logo added

---
+++ src/components/Logo.tsx
@@ -0,0 +1,9 @@
+export const Logo: React.FC = () => (
+    <div style={{fontSize: 25, paddingLeft: 10, paddingTop: 2}}>
+        <span style={{opacity: 0.5}}>my</span>
+        <span style={{fontWeight: "bold"}}>ml</span>
+        <span style={{fontWeight: "bold"}}>practice</span>
+        <span style={{fontWeight: "bold", color: "#F54900FF"}}>.dev</span>
+    </div>
+
+)
