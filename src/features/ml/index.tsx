0901675128bdf21ed43a311f1666c75260bde5a2
Oleg Selivantsev <o.selivancev@gmail.com>
2026-01-28 01:49:17 +0400

    upd

--- src/features/ml/index.tsx
+++ src/features/ml/index.tsx
@@ -1,5 +1,5 @@
 import ReCharts from "./ReCharts.tsx";
-import {TrainingsContext, LoadingContext} from "../../context.ts";
+import {TrainingsContext, LoadingContext} from "@/context.ts";
 import {useState} from "react";
 
 export const ML = () => {
@@ -7,6 +7,16 @@ export const ML = () => {
     const [loading, setLoading] = useState({
         status: false, text: ''
     });
+
+    {/*<div className="flex flex-1 flex-col gap-4 p-4">*/}
+    {/*    <div className="grid auto-rows-min gap-4 md:grid-cols-3">*/}
+    {/*        <div className="bg-muted/50 aspect-video rounded-xl" />*/}
+    {/*        <div className="bg-muted/50 aspect-video rounded-xl" />*/}
+    {/*        <div className="bg-muted/50 aspect-video rounded-xl" />*/}
+    {/*    </div>*/}
+    {/*    <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />*/}
+    {/*</div>*/}
+
 
     return (
         <TrainingsContext.Provider value={{trainings, setTrainings}}>
